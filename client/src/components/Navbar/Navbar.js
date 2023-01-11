0a352e403ec2c84c09b005128a1462105baf814b
Tristan Barron <tristan_wilson_63@yahoo.com>
2023-01-11 14:17:48 -0600

    navbar

---
+++ client/src/components/Navbar/Navbar.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Navbar.css";
+import { Navbar, Nav, Container, Modal, Tab, NavbarBrand } from 'react-bootstrap';
+
+
+function NavBar() {
+    return (
+<Navbar className="sidebar">
+    <NavbarBrand>
+        The Townies
+    </NavbarBrand>
+</Navbar>
+    );
+}
+
+export default NavBar;
