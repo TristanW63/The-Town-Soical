cf48c7a7ccc6c888c8b0da1db398f60beb812728
Tristan Barron <tristan_wilson_63@yahoo.com>
2023-01-11 15:58:39 -0600

    login/register

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,19 @@
+import React, { useState } from 'react';
 import './App.css';
-import Navbar from "./components/Navbar/Navbar";
+import { Login } from './components/Login';
+import { Register } from './components/Register';
 
 function App() {
+  const [currentForm, setCurrentForm] = useState( 'login');
+
+  const toggleForm = (formName) => {
+    setCurrentForm(formName)
+  }
   return (
     <div className="App">
-     <Navbar />
+     {
+      currentForm === "login" ? <Login onFormSwitch={toggleForm} /> : <Register onFormSwitch={toggleForm}/>
+      }
     </div>
   );
 }
--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./Navbar.css";
-import { Navbar, Nav, Container, Modal, Tab, NavbarBrand } from 'react-bootstrap';
+import { Navbar, Nav, Container, Modal, Tab, NavbarBrand, NavLink } from 'react-bootstrap';
 
 
 function NavBar() {
@@ -9,7 +9,38 @@ function NavBar() {
     <NavbarBrand>
         The Townies
     </NavbarBrand>
-    
+    <Nav>
+        <Nav.Item>
+            <NavLink>
+                Home
+            </NavLink>
+        </Nav.Item>
+        <Nav.Item>
+            <NavLink>
+                Search
+            </NavLink>
+        </Nav.Item>
+        <Nav.Item>
+            <NavLink>
+                Profile
+            </NavLink>
+        </Nav.Item>
+        <Nav.Item>
+            <NavLink>
+                Create Post
+            </NavLink>
+        </Nav.Item>
+        <Nav.Item>
+            <NavLink>
+                Likes
+            </NavLink>
+        </Nav.Item>
+        <Nav.Item>
+            <NavLink>
+                Logout
+            </NavLink>
+        </Nav.Item>
+    </Nav>
 </Navbar>
     );
 }
