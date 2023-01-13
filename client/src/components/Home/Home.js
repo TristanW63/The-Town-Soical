bf4caded2f0f193dce222dcf2cbb5eeea5872690
Tristan Barron <tristan_wilson_63@yahoo.com>
2023-01-12 20:35:06 -0600

    home page

---
+++ client/src/components/Home/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Home.css"
+
+const Home = () => {
+
+    return (
+        <div className="App">
+<div className="boxes">
+    <p>fuckyou</p>
+</div>
+        </div>
+    );
+};
+
+export default Home;
