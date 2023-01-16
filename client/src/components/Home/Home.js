cfefc65fef35f6b548da1af73b09f85672d7339d
Tristan Barron <tristan_wilson_63@yahoo.com>
2023-01-16 17:52:01 -0600

    nav and post modal

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,14 +1,13 @@
 import React from "react";
 import "./Home.css";
 import NavBar from "./../Navbar/Navbar";
-import Post from "../Post/Post";
 
 const Home = () => {
   return (
     <div className="App">
       <NavBar />
       <div className="boxes">
-        <Post />
+        yes
       </div>
     </div>
   );
